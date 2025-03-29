e7c1f69d9a27e148a0db870f288e3ac3cde85b64
Seonghun Kang <lutis107@gmail.com>
2025-03-29 18:34:34 +0900

    add: MusicEntity

---
+++ music-app/src/main/kotlin/kr/co/music/domain/MusicEntity.kt
@@ -0,0 +1,43 @@
+package kr.co.music.domain
+
+import jakarta.persistence.*
+import kr.co.music.domain.common.BaseEntity
+
+@Entity
+@Table(name = "MUSIC")
+class MusicEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "music_id")
+    val musicId: Long? = null,
+
+    @Column(nullable = false)
+    val title: String,
+
+    @Column(nullable = false)
+    val artist: String
+) : BaseEntity() {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as MusicEntity
+
+        if (musicId != other.musicId) return false
+        if (title != other.title) return false
+        if (artist != other.artist) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = musicId?.hashCode() ?: 0
+        result = 31 * result + title.hashCode()
+        result = 31 * result + artist.hashCode()
+        return result
+    }
+
+    override fun toString(): String {
+        return "MusicEntity(musicId=$musicId, title='$title', artist='$artist')"
+    }
+}
